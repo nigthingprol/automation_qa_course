5999326815aa2067dc6b22c38d147c3e1725e4f6
lenovo <nalivayko_2018@inbox.ru>
2025-02-11 20:59:46 +0700

    test

---
+++ pages/elements_page.py
@@ -0,0 +1,21 @@
+import time
+
+from locators.elements_page_locators import TextBoxPageLocators
+from pages.base_page import BasePage
+
+class TextBoxPage(BasePage):
+    locators = TextBoxPageLocators()
+
+    def fill_all_fields(self, ):
+        self.element_is_visible(self.locators.FULL_NAME).send_keys('maxim')
+        self.element_is_visible(self.locators.EMAIL).send_keys('maxim@aaa.ru')
+        self.element_is_visible(self.locators.CURRENT_ADDRESS).send_keys('maxim')
+        self.element_is_visible(self.locators.PERMANENT_ADDRESS).send_keys('maxim')
+        self.element_is_visible(self.locators.SUBMIT).click()
+
+    def check_field_form(self):
+        full_name = self.element_is_present(self.locators.CREATED_FULL_NAME).text.split(':')[1]
+        email = self.element_is_present(self.locators.CREATED_EMAIL).text.split(':')[1]
+        current_address = self.element_is_present(self.locators.CREATED_CURRENT_ADDRESS).text.split(':')[1]
+        permanent_address = self.element_is_present(self.locators.CREATED_PERMANENT_ADDRESS).text.split(':')[1]
+        return full_name, email, current_address, permanent_address
--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,8 +1,17 @@
 import time
 from pages.base_page import BasePage
+from pages.elements_page import TextBoxPage
 
 
-def test(driver):
-    page = BasePage(driver, 'https://webiomed.ru/')
-    page.open()
-    time.sleep(3)
+class TestElements:
+    class TestTextBox:
+
+        def test_text_box(self, driver):
+            text_box_page = TextBoxPage(driver, 'https://demoqa.com/text-box')
+            text_box_page.open()
+            text_box_page.fill_all_fields()
+            output_name, output_email, output_cur_address, output_per_addr = text_box_page.check_field_form()
+            print(output_name)
+            print(output_email)
+            print(output_cur_address)
+            print(output_per_addr)
